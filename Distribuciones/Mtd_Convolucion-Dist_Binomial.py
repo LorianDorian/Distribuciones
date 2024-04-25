00e06b2131f30f61685b55b64af16762eb2a2df1
01Shepherd <167943693+01Shepherd@users.noreply.github.com>
2024-04-24 20:51:19 -0600

    SolofaltaFaty-Mtd_Convolucion-Dist_Binomial.py

--- Distribuciones/Mtd_Convolucion-Dist_Binomial.py
+++ Distribuciones/Mtd_Convolucion-Dist_Binomial.py
@@ -1,20 +1,79 @@
 import pandas as pd
+import math
+import numpy
 
-Ri = [0.02, 0.31, 0.70, 0.83, 0.50]
+#distribucion exponencial
+# binomial
+#triangular
 
-# Probabilidad
-p = 0.03
+def exponencial (riList, media):
+    resultsList = []
+    variables = []
+    variable = 0
+    for num in riList:
+        result = -media * numpy.log(1- num)
+        resultsList.append(result)
+        variables.append(variable)
+        variable = variable + 1
 
-# Crear un DataFrame que almacena los valores de ri y su comparación, y los imprime en forma de tabla acá bien bonito
-df = pd.DataFrame(Ri, columns=['Ri'])
+    for i in variables:
+        print(f"Variable {i}    Ri = {riList[i]}  Resultado:   {resultsList[i]}")
 
-# fórmula para determinar si cada pieza es defectuosa
-df['BE'] = df['Ri'].apply(lambda ri: 1 if ri > (1 - p) else 0)
+def binomial(riList, probabilidad):
+        Ri = riList
 
-# Contar el número de piezas defectuosas
-defectuosas = df['BE'].sum()
+        # Probabilidad
+        p = probabilidad
 
-# Imprimir el DataFrame y el número total de piezas defectuosas
-print(df)
-print(f"Número total de piezas defectuosas: {defectuosas}")
+        # Crear un DataFrame que almacena los valores de ri y su comparación, y los imprime en forma de tabla acá bien bonito
+        df = pd.DataFrame(Ri, columns=['Ri'])
 
+        # fórmula para determinar si cada pieza es defectuosa
+        df['BE'] = df['Ri'].apply(lambda ri: 1 if ri > (1 - p) else 0)
+
+        # Contar el número de piezas defectuosas
+        defectuosas = df['BE'].sum()
+
+        # Imprimir el DataFrame y el número total de piezas defectuosas
+        print(df)
+        print(f"Número total de piezas defectuosas: {defectuosas}")
+
+
+def main(): 
+
+        print("Elige un metodo: ")
+        print("1. Exponencial   2.Triangular    3.Binomial")
+        metodoSeleccionado = int(input())
+
+        if metodoSeleccionado == 1: 
+            listaRi = []
+            print("ingresa la cantidad de numeros ri")
+            cantidadRi = int(input())
+
+            print("Ingrsa uno por uno los numeros")
+            num = 1
+            while num <= cantidadRi: 
+                listaRi.append(float(input(num)))
+                num = num +1
+            print("Ingresa la media")
+            media = float(input())
+
+            exponencial(listaRi, media)
+        
+        if metodoSeleccionado == 3:
+             listaRi = []
+             print("ingresa la cantidad de numeros ri")
+             cantidadRi = int(input())
+             print("Ingrsa uno por uno los numeros")
+             num = 1
+             while num <= cantidadRi: 
+                listaRi.append(float(input(num)))
+                num = num +1
+             print("Ingresa la probabilidad")
+             probabilidad = float(input())
+
+             binomial(listaRi, probabilidad)
+             
+
+if __name__ == "__main__":
+    main()
